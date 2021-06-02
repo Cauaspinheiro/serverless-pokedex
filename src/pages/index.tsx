7c7f05fac64487afe18213266b0cc582acbaf309
Cauã Pinheiro <cauaspinheiro@gmail.com>
2021-06-01 23:39:17 -0300

    :sparkles: add home page UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,31 @@ const HomePage: FC = () => {
   return (
     <div className="container">
       <Head>
-        <title>Serverless Pokedex</title>
+        <title>Serverless Pokédex</title>
+
+        <link rel="icon" href="/favicon.ico" />
+
+        <meta name="robots" content="noindex" />
       </Head>
 
-      <h1>Serverless Pokedex</h1>
+      <div className="content">
+        <a
+          className="title"
+          href="https://github.com/Cauaspinheiro/serverless-pokedex"
+        >
+          <h1>Serverless Pokédex</h1>
+        </a>
+
+        <img src="/charmander.gif" alt="Charmander" />
+
+        <a
+          className="image-credits"
+          href="https://www.behance.net/gallery/41494551/Charmander?tracking_source=search_projects_recommended%7Cpokemon"
+          rel="noopener noreferrer"
+        >
+          Art by Ashley Odell on Behance
+        </a>
+      </div>
     </div>
   );
 };
